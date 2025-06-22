901a16470752b2196b0bce00ce66a89a930703ec
Thais Costa <thais1fc2002@gmail.com>
2025-06-22 01:20:46 -0300

    unindo as duas screens

---
+++ src/screens/LetterBoxScreen/index.tsx
@@ -0,0 +1,65 @@
+import { StatusBar } from 'expo-status-bar';
+import { View, Text, Image, TouchableOpacity, TextInput, FlatList } from 'react-native';
+import { styles } from './styles';
+
+const FILMS = [
+  { id: 1, image: require('./assets/liloStitch.png') },
+  { id: 2, image: require('./assets/mickey17.png') },
+  { id: 3, image: require('./assets/interstellar.png') },
+  { id: 4, image: require('./assets/materialist.png') },
+  { id: 5, image: require('./assets/sinners.png') },
+  { id: 6, image: require('./assets/darkNight.png') },
+  { id: 7, image: require('./assets/homemComH.png') },
+  { id: 8, image: require('./assets/inception.png') },
+  { id: 9, image: require('./assets/RitaLee.png') },
+  { id: 10, image: require('./assets/UltimaGota.png') },
+  { id: 11, image: require('./assets/kpopHunters.png') },
+  { id: 12, image: require('./assets/untilDawn.png') },
+];
+
+
+
+
+
+
+export default function LetterBox() {
+  return (
+
+    <View >
+      <View style={styles.Header}>
+        <Text style={styles.title}>Popular</Text>
+        <TouchableOpacity style={styles.butonnBusca} >
+          <Text style={styles.lupa}>🔎</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View >
+        <TouchableOpacity style={styles.menus}>
+          <Text style={styles.text}>FILMS</Text>
+          <Text style={styles.text}>REVIEWS</Text>
+          <Text style={styles.text}>LISTS</Text>
+          <Text style={styles.text}>JOURNAL</Text>
+        </TouchableOpacity>
+      </View >
+
+      <FlatList
+        data={FILMS}
+        keyExtractor={(item) => item.id.toString()}
+        numColumns={3}
+        contentContainerStyle={styles.grid}
+        renderItem={({ item }) => (
+          <View style={styles.card}>
+            <Image source={item.image} style={styles.image} />
+          </View>
+        )}
+
+      />
+
+
+
+
+    </View >
+
+  );
+}
+
---
+++ src/screens/SantanderScreen/index.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { View, Text, Image, TouchableOpacity, TextInput } from "react-native";
+import { styles } from "../SantanderScreen/styles";
+import Santanderlogo from ".../../../../TrabalhoIndividualRN/src/assets/SantanderLogo.png";
+
+
+export const Santander = () => {
+  return (
+    <View style={styles.container}>
+      <Image source={Santanderlogo} style={styles.logo} />
+      <Text style={styles.title}>Estamos quase lá...</Text>
+
+      <View style={styles.cpfContainer}>
+        <Text style={styles.cpfText}>CPF:***.***.***-10</Text>
+        <Text style={styles.cpfLapis}>✏️</Text>
+      </View>
+
+      <Text style={styles.senhaAcesso}>Digite a senha de acesso</Text>
+
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder=""
+          secureTextEntry={true}
+          style={styles.input}
+        />
+        <Text style={styles.olho}>👁️</Text>
+      </View>
+
+      <TouchableOpacity onPress={() => { console.log("Teste!!") }} style={styles.esqueceuContainer}>
+        <Text style={styles.chave}>🔑</Text>
+        <Text style={styles.esqueceu}>Esqueceu sua senha?</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Entrar</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
